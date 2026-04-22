aad1e0bc05887ffef66a692d9aed98a8afd0164a
Fatima-Rahmani79 <fatima.rahmani79@gmail.com>
2026-04-22 15:37:43 +0430

    add SettingsContext and provider for managing settings state

---
+++ src/context/SettingsContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useReducer, useContext } from "react";
+import { settingsReducer, initialState } from "./settingsReducer";
+
+const SettingsContext = createContext();
+
+export const SettingsProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(settingsReducer, initialState);
+
+  return (
+    <SettingsContext.Provider value={{ state, dispatch }}>
+      {children}
+    </SettingsContext.Provider>
+  );
+};
+
+export const useSettings = () => useContext(SettingsContext);
