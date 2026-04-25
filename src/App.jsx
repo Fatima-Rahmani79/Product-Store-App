1e28c5547a85f094c73c54f92fb0159608ab684f
Fatima-Rahmani79 <fatima.rahmani79@gmail.com>
2026-04-25 11:06:34 +0430

    feat: wrap application in ThemeProvider for theme support

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import { getTheme } from "./theme/theme";
+import { useSettings } from "./context/SettingsContext";
 
 function App() {
+  const { state } = useSettings();
+
+  const theme = getTheme(state.theme);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/cart" element={<CartPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
